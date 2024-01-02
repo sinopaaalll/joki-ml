f0d9daa1e101fab5312a805359f0fd460d2722b2
dixkass <dixkass08@gmail.com>
2024-01-03 06:00:03 +0700

    80%

--- database/joki_ml.sql
+++ database/joki_ml.sql
@@ -2,8 +2,8 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Dec 26, 2023 at 08:17 PM
+-- Host: 127.0.0.1
+-- Generation Time: Jan 02, 2024 at 11:50 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 5.6.40
 
@@ -60,12 +60,45 @@ CREATE TABLE `layanan` (
 --
 
 INSERT INTO `layanan` (`id`, `nama_layanan`, `kategori_id`, `harga`, `foto`) VALUES
-(1, 'Master', 1, 3000, '86cc29a14fb10514845a421c63ab66bc.jpg'),
-(2, 'Grand Master', 1, 5000, '0f65dc5c78d26cb756dc4923f52345d3.jpg'),
-(3, 'Epic', 1, 7000, 'e27e4df5b4225dc0448dafb42aa459b7.jpg'),
-(4, 'Legend V - Mythic ', 3, 200000, '9b4efe22940d6c52d96d92f597e54ce4.png'),
-(5, 'Legend', 1, 10000, 'd33349dffb3289c0dea3dddd014972c9.jpg'),
-(6, 'Mythic', 1, 15000, '78a266b2762ce22c74563ae53792055d.jpg');
+(1, 'Master', 1, 3000, '5bbd07f5f962562d1c8724fbd3e765cf.png'),
+(2, 'Grand Master', 1, 5000, '320f9ea6e190c8d9ee82644eb53bea99.png'),
+(3, 'Epic', 1, 7000, '88259095d86dff074415f06450567c0c.png'),
+(4, 'Legend V - Mythic ', 3, 200000, '0e0efdaad9e3bbcf24667b818e28ee74.png'),
+(5, 'Legend', 1, 10000, '663e8859317df35bb35885d338fb3db3.png'),
+(6, 'Mythic', 1, 15000, '1667db9946e0f7ec54b6d77d14eb5582.png'),
+(7, 'Mythic Honor', 1, 25000, '127582cb60d856930df96104fff17ee5.png'),
+(8, 'Mythic Glory', 1, 30000, 'c28a5caaab9206d1784d7c48886fcfc8.png'),
+(9, 'Mytich Immortal', 1, 35000, '1460cccf28d41900c46e3520a55b2087.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id` int(11) NOT NULL,
+  `login_via` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `hero` varchar(50) NOT NULL,
+  `nick_name` varchar(50) NOT NULL,
+  `jumlah_star` int(11) NOT NULL,
+  `catatan` varchar(150) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `no_wa` varchar(20) NOT NULL,
+  `created_at` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pesanan`
+--
+
+INSERT INTO `pesanan` (`id`, `login_via`, `email`, `password`, `hero`, `nick_name`, `jumlah_star`, `catatan`, `nama`, `no_wa`, `created_at`) VALUES
+(28, 'Montoon', 'sasaaaa12@gmail.com', '1234sqw', 'chou', '1221375 (OLA)', 5, 'klasdhodq', 'Asepr', '08124782243', '2024-01-02'),
+(29, 'Montoon', 'assaklfk@gmail.com', '1234qeq', 'Dada', '121212 (onti)', 0, 'sohfwlf', 'sadjbfhjfw', '088192465', '2024-01-03'),
+(30, 'Montoon', '131', '13', '113', '123', 10, '31', 'amdla', '02809353563', '2024-01-03'),
+(31, 'Montoon', '123', '141', '123', '224', 10, '1231', 'nlwhf', '088935831', '2024-01-04');
 
 --
 -- Indexes for dumped tables
@@ -85,6 +118,12 @@ ALTER TABLE `layanan`
   ADD KEY `fk_kategori` (`kategori_id`);
 
 --
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -98,7 +137,13 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `layanan`
 --
 ALTER TABLE `layanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- Constraints for dumped tables
